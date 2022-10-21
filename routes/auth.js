336a86c351e3280c6cde97e0163b668068ea0c53
vishnu <vishnu@sedintechnologies.com>
2022-10-21 18:46:52 +0530

    new

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ router.get("/api/ping", (request, response) => {
   response.status(200).json({ message: "pong" });
 });
 
-router.post("/api/send-reset-password-email", (request, response) => {
+router.post("/api/users/forgot-password", (request, response) => {
   const result = authController.forgotPassword(request.body);
   response.set("Access-Control-Allow-Origin", "http://localhost:4200/");
   response.status(result.status).json(result);
@@ -17,7 +17,7 @@ router.post("/api/login", (request, response) => {
   response.set("Access-Control-Allow-Origin", "http://localhost:4200/");
   response.status(result.status).json(result);
 });
-router.post("/api/reset-password/:token", (request, response) => {
+router.post("/api/users/reset-password/:token", (request, response) => {
   response.set("Access-Control-Allow-Origin", "http://localhost:4200/");
   const result = authController.resetPassword({
     ...request.body,
@@ -26,39 +26,39 @@ router.post("/api/reset-password/:token", (request, response) => {
   response.status(result.status).json(result);
 });
 
-router.post("/api/verify-user/:uid/:token", (request, response) => {
+router.post("/api/users/verify-token/:uid/:token", (request, response) => {
   response.set("Access-Control-Allow-Origin", "http://localhost:4200/");
   const result = authController.verifyUser(request.params);
   response.status(result.status).json(result);
 });
 
-router.get("/api/collaborators-search", (request, response) => {
-  response.set("Access-Control-Allow-Origin", "http://localhost:4200/");
+router.get('/api/collaborations/search', (request, response) => {
+  response.set('Access-Control-Allow-Origin', 'http://localhost:4200/');
   const result = authController.listApi(request.query);
   response.status(200).json({ ...result });
 });
-router.get("/api/documents-search", (request, response) => {
+router.get("/api/documents/search", (request, response) => {
   response.set("Access-Control-Allow-Origin", "http://localhost:4200/");
   const result = authController.documentApi(request.query);
   response.status(200).json({ ...result });
 });
 
-router.get("/api/get-default-filter-values", (request, response) => {
-  response.set("Access-Control-Allow-Origin", "http://localhost:4200/");
+router.get('/api/collaborations/filter-values', (request, response) => {
+  response.set('Access-Control-Allow-Origin', 'http://localhost:4200/');
   const result = authController.getAllFilters();
-  response.status(200).json({...result });
+  response.status(200).json({ ...result });
 });
 
-router.get("/api/user", (request, response) => {
-  response.set("Access-Control-Allow-Origin", "http://localhost:4200/");
-  const result = authController.getUser(request.query);
+router.get('/api/providers/:id', (request, response) => {
+  response.set('Access-Control-Allow-Origin', 'http://localhost:4200/');
+  const result = authController.getUser(request.params);
   response.status(200).json(result);
 });
-
 
 // CRUD for admin/super admin users
 router.post('/api/zivianusers', (request, response) => {
   response.set('Access-Control-Allow-Origin', 'http://localhost:4200/');
+  authController.createUser(request.body);
   response.status(200).json({ message: 'User created'});
 });
 
@@ -67,4 +67,23 @@ router.get('/api/zivianusers', (request, response) => {
   const result = authController.usersApi(request.query);
   response.status(200).json({ ...result });
 });
+
+router.get('/api/zivianusers/:id', (request, response) => {
+  response.set('Access-Control-Allow-Origin', 'http://localhost:4200/');
+  const result = authController.usersApi(request.params);
+  response.status(200).json({ ...result });
+});
+
+router.delete('/api/zivianusers/:id', (request, response) => {
+  response.set('Access-Control-Allow-Origin', 'http://localhost:4200/');
+  response.status(200).json({ message: 'User Deleted' });
+});
+
+router.put('/api/zivianusers/:id', (request, response) => {
+  response.set('Access-Control-Allow-Origin', 'http://localhost:4200/');
+  authController.updateUser(request.body);
+  response.status(200).json({ message: 'User updated' });
+});
+
+
 module.exports = router;
